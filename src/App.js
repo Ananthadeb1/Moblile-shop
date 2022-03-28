aa90cd1bdf7ae2b03b9d8eefe9d1b026a3a6cd42
Anantha debnath <88941537+Ananthadeb1@users.noreply.github.com>
2022-03-28 23:21:29 +0600

    sob sesh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import { useEffect, useState } from 'react';
+import Shop from './components/shop/Shop';
+import Header from './components/header/Header';
+
 
 function App() {
   return (
-    <div>hi there</div>
+    <div className="App">
+      <Header></Header>
+      <Shop></Shop>
+    </div>
   );
-};
+}
 
 export default App;
